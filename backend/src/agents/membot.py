4419276b8b224fa1fa36384323c222072aff3c4f
Zavier Kamath <zavierkamath@gmail.com>
2026-05-06 10:12:04 -0400

    added kb search tool to membot

--- backend/src/agents/membot.py
+++ backend/src/agents/membot.py
@@ -1,6 +1,7 @@
 from src.agents.pipedagent import PipedAgent
 from src.prompts import MEMBOT_SYSTEM_PROMPT
 # from src.tools.spawn_resumebot import spawn_resumebot_serviceless
+from src.tools.kb_search import kb_search as kb_search_prewrap
 
 from pydantic_ai.common_tools.duckduckgo import duckduckgo_search_tool
 from pydantic_ai import Tool
@@ -16,6 +17,12 @@ class MemBot(PipedAgent):
         #     description="Spawns ResumeBot with the detailed instructions that are given as input. ResumeBot will respond in natural language.",
         # )
 
+        kb_search = Tool(
+            kb_search_prewrap,
+            name="kb_search",
+            description="Searches the knowledgebase of memories for memories that are semantically similar to a query. Returns the top k most relevant results.",
+        )
+
         super().__init__(
             agent_name="membot",
             description="Primary Chatbot for Memvia: a service that gives a user the ability to manage and chat with memories, particularly in the realm of maintaining knowledge of their professional life for the sake of job hunting applicatons.",
@@ -23,6 +30,7 @@ class MemBot(PipedAgent):
             session=job_id,
             tools=[
                 duckduckgo_search_tool(max_results=5),
+                kb_search,
                 # spawn_resumebot,
             ],
         )
